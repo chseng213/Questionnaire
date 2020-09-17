8fff1f36900e6e3ca8804b9350552315a338795f
chseng <chseng213@163.com>
2020-09-17 17:38:18 +0800

    feat:跨域问题

--- ext_app.py
+++ ext_app.py
@@ -1,9 +1,11 @@
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
 def init_ext(app):
     # 初始化数据库
     init_db(app)
+    init_cors(app)
 
 
 db = SQLAlchemy()
@@ -12,3 +14,9 @@ db = SQLAlchemy()
 # 初始化数据库
 def init_db(app):
     db.init_app(app)
+
+
+cors = CORS()
+
+def init_cors(app):
+    cors.init_app(app)

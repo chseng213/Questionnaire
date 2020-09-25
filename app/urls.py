63b12471c887ef0574dc22f5d165b0d98a4dce26
chseng <chseng213@163.com>
2020-09-25 16:59:43 +0800

    add: 新增接口路由

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
-from flask_restful import Api
+from app.api import QuestionResource, EChartResource, QuestionnaireResource, CorrectScores, CorrectScoresRank
+from app.utils import ExtendedAPI
 
-from app.api import QuestionResource, EChartResource, QuestionnaireResource
-
-api = Api()
+api = ExtendedAPI()
 
 api.add_resource(QuestionResource, '/questionnaire', endpoint='question')
 api.add_resource(EChartResource, '/questionnaire/<string:questionnaire_id>/result', endpoint='result')
 api.add_resource(QuestionnaireResource, '/questionnaire/add', endpoint='questionnaire')
+# Correct Score
+api.add_resource(CorrectScores, '/jcrace/<int:race_id>', endpoint='correct_scores')
+api.add_resource(CorrectScoresRank, '/jcrace/<int:race_id>/ranking', endpoint='ranking')
 
 
 def init_api(app):
